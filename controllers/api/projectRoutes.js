674ba57d6c700f4ddbec4b8067cd5b9d55b5820e
petercolella <peter.joseph.colella@gmail.com>
2021-05-01 12:59:06 -0400

    updates projectRoutes.js with Multer logic

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -1,16 +1,36 @@
 const router = require('express').Router();
 const { Project } = require('../../models');
 const withAuth = require('../../utils/auth');
+const multer = require('multer');
 
-router.post('/', withAuth, async (req, res) => {
+// From Multer documentation: https://github.com/expressjs/multer#diskstorage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, __dirname + '../../../public/images/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
+
+// The upload.single() middleware is used when a single file is expected:
+// https://github.com/expressjs/multer#singlefieldname
+router.post('/', withAuth, upload.single('image'), async (req, res) => {
+  // req.file is the 'image' file.
+  console.log('req.file', req.file);
+
   try {
     const newProject = await Project.create({
       ...req.body,
+      // If no image was uploaded, undefined is returned, and the project is created with the default image.
+      image: req.file && req.file.originalname,
       user_id: req.session.user_id,
     });
 
     res.status(200).json(newProject);
   } catch (err) {
+    console.log(err);
     res.status(400).json(err);
   }
 });
